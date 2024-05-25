69e67d053c10c2965d2d12009914569385a8a881
ggang89 <ggang89@gmail.com>
2024-05-25 16:09:22 +0900

    createBrowser Rputer 기본 구조

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        홈!!!!
+        <div>
+          <Link to="/about">어바웃으로</Link>
+        </div>
+      </div>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        about
+        <div>
+          <Link to="/">home</Link>
+        </div>
+      </div>
+    ),
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
